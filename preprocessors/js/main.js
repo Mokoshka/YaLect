13a8ea0eb95987ed0e522d8941e71430833a2227
Aleksander Zavyalov <hanky@yandex-team.ru>
2016-03-17 14:54:44 +0500

    Видео суперкота

--- preprocessors/js/main.js
+++ preprocessors/js/main.js
@@ -1,6 +1,6 @@
 // запуск мохова
-(function () {
-    var $section = $('#postcss_bem'),
+(function() {
+    var $section = $('#postcss-bem'),
         $mokhov = $('#mokhov'),
         $body = $('body');
 
@@ -8,16 +8,49 @@
 
     $section.attrchange({
         trackValues: true,
-        callback: function (event) {
-            if (event.attributeName !== 'class')
+        callback: function(event) {
+            if (event.attributeName !== 'class') {
                 return;
-
-            if (/present/.test(event.newValue)) {
-                $mokhov.show();
-                $mokhov.css('animation-name', 'move_mokhov');
-            } else {
-                $mokhov.hide();
             }
+
+            if (!/present/.test(event.newValue)) {
+                $mokhov.hide();
+                $mokhov.css('animation-name', 'null');
+                return;
+            }
+
+            $mokhov.show();
+            $mokhov.css('animation-name', 'move_mokhov');
+        }
+    });
+}());
+
+// запуск видео с котом-героем
+(function() {
+    var $section = $('#css-needs-hero'),
+        $video = $('#supercat'),
+        video = $video[0],
+        $body = $('body');
+
+    $video.appendTo($body);
+
+    $section.attrchange({
+        trackValues: true,
+        callback: function(event) {
+            if (event.attributeName !== 'class') {
+                return;
+            }
+
+            if (!/present/.test(event.newValue)) {
+                video.pause();
+                video.currentTime = 0;
+                $video.hide();
+                return;
+            }
+
+            $video.show();
+            // запускаем видео
+            video.play();
         }
     });
 }());
