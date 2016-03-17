b4db212255f708e6630e5c0ffb92a9776174f770
Aleksander Zavyalov <hanky@yandex-team.ru>
2016-03-17 15:07:25 +0500

    Добавление видео пылесоса

--- preprocessors/js/main.js
+++ preprocessors/js/main.js
@@ -27,8 +27,38 @@
 
 // запуск видео с котом-героем
 (function() {
-    var $section = $('#css-needs-hero'),
-        $video = $('#supercat'),
+    var $section = $('#supercat'),
+        $video = $('#supercat-video'),
+        video = $video[0],
+        $body = $('body');
+
+    $video.appendTo($body);
+
+    $section.attrchange({
+        trackValues: true,
+        callback: function(event) {
+            if (event.attributeName !== 'class') {
+                return;
+            }
+
+            if (!/present/.test(event.newValue)) {
+                video.pause();
+                video.currentTime = 0;
+                $video.hide();
+                return;
+            }
+
+            $video.show();
+            // запускаем видео
+            video.play();
+        }
+    });
+}());
+
+// запуск видео с кокаином
+(function() {
+    var $section = $('#cocaine'),
+        $video = $('#cocaine-video'),
         video = $video[0],
         $body = $('body');
 
